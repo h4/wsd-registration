cbbaa18afdbed94452888ce0aa42e5ac91eb1f39
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-06-17 02:18:34 +0400

    Все json-ответы c кодом 200

--- views/error.js
+++ views/error.js
@@ -1,13 +1,14 @@
 "use strict";
 
 function errorView(res, code, message) {
-    res.writeHead(code, {
+    res.writeHead(200, {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json'
     });
 
     var dataObj = {
         result: "error",
+        code: code,
         message: message
     };
 
